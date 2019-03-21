6096feba7cc248a3706769e9d264c7ad6059fca4
jenkins <13413607283@163.com>
2019-03-21 08:34:17 +0800

    33

--- server/src/jenking/com/controller/IndexController.java
+++ server/src/jenking/com/controller/IndexController.java
@@ -9,8 +9,8 @@ import com.jfinal.core.Controller;
 public class IndexController extends Controller {
 	
 	public void index(){
-//		render("/index.jsp");
-		renderText("Hello JFinal World.");
+		render("/index.jsp");
+//		renderText("Hello JFinal World.");
 	}
 	
 }
